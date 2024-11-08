fcd511d14e78c305efe6de301909de60c92b32e2
Umme Salma Papon <papon.fsp@gmail.com>
2024-11-08 18:28:56 +0200

    problem 8 has been fixed

---
+++ problem-8.ts
@@ -0,0 +1,23 @@
+/***
+ * --------------------------------------------------------------------------------------
+ * Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+
+      => Sample Input:
+      --------------------
+      const person = { name: "Alice", age: 25, email: "alice@example.com" };
+      console.log(validateKeys(person, ["name", "age"]));
+
+      => Sample Output:
+      ---------------------
+      true;
+    -------------------------------------------------------------------------------------
+ */
+
+    const validateKeys = <T>(obj : T, keys: (keyof T) []): boolean => {
+      return keys.every(key => key in obj);
+      
+
+    }
+
+   const person = { name: "Alice", age: 25, email: "alice@example.com" };
+   console.log(validateKeys(person, ["name", "age"]));
